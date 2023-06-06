c24133766e4eab26a74747b0c9d935583e737f17
Thiago Almeida <t.leandroalmeida@gmail.com>
2023-06-06 08:08:29 -0300

    FeignClient

--- hr-payroll/src/main/java/com/msbootcloud/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/msbootcloud/hrpayroll/services/PaymentService.java
@@ -1,35 +1,23 @@
 package com.msbootcloud.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.msbootcloud.hrpayroll.entities.Payment;
 import com.msbootcloud.hrpayroll.entities.Worker;
+import com.msbootcloud.hrpayroll.feignclients.WorkerFeignClient;
 
 /* Registrar o serviço como um componente do Spring para que seja
  * injetado em outras classes */
 @Service
 public class PaymentService {
-
-	@Value("${hr-worker.host}")
-	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayments(long workerId, int days) {
 		
-		/* Mapa ou dicionário de parâmetros para passar como url na
-		 * chamada do queryForObject */
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
