96c7976c430a7483c1a334621e83aaca8dae4763
Thiago Almeida <t.leandroalmeida@gmail.com>
2023-06-06 08:25:46 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/msbootcloud/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/msbootcloud/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.msbootcloud.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +25,11 @@ import com.msbootcloud.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
+	
 	/*Objeto que acessa o banco de dados
 	 * Injeção automática de dependência */
 	@Autowired
@@ -37,6 +45,8 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		/*.get pega o objeto dentro do Optional*/
 		Worker obj = repository.findById(id).get(); 
