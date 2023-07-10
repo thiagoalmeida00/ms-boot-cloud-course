8197d1f821efd24871ff6c3af7361b98f45c74fb
Thiago Almeida <t.leandroalmeida@gmail.com>
2023-07-10 08:49:29 -0300

    Configuring hr-config-server project

---
+++ hr-config-server/src/test/java/com/msbootcloud/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msbootcloud.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
